0b0dea0aa6d51d2f31ef8e5c00c05928da079316
Raymond <raymondafrani00@gmail.com>
2026-09-23 21:39:03 +0000

    changed the url for render

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [loading ,setLoading] = useState(false);
 
-  const apiUrl = import.meta.env.VITE_API_URL || "https://portfolio-l3r4.onrender.com";
+  const apiUrl = import.meta.env.VITE_API_URL || "https://portfolio-shgm.onrender.com";
 
   const handleSubmit = async (e) => {
     e.preventDefault();
